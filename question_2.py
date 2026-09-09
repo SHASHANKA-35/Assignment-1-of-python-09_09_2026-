7a5a8389e46729eeb5f7c5c88d9c4df790f7c249
SHASHANKA-35 <2026uue0372@iitjammu.ac.in>
2026-09-10 00:09:27 +0530

    Question 2 is done.

---
+++ question_2.py
@@ -0,0 +1,8 @@
+a=int(input("Enter a number:"))
+b=int(input("Enter another number:"))
+if(a==b):
+    print("The numbers are equal.")
+elif(a>b):
+    print("The first number is greater than the second number.")
+else:
+    print("The second number is greater than the first number.")
